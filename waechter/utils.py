062fedc5ca177972160e2a7ca4e1e7d22e739e56
glaslos <lukas.rist@bluecoat.com>
2015-02-24 11:06:52 +0100

    minor improvements on the influx reporter

--- waechter/utils.py
+++ waechter/utils.py
@@ -4,14 +4,16 @@ import requests
 from pprint import pprint
 
 
-def report_data(data_points, influx_host):
+def report_data(data_points, name, columns, influx_host='localhost', debug=False):
     stats = list()
     for key, value in data_points.items():
         stats.append([key, value])
     data = {
-        "name": "asgard.queue.gauges",
-        "columns": ["queue_name", "value"],
-        "points": stats
+        'name': name,
+        'columns': columns,
+        'points': stats
     }
-    pprint(data)
+    if debug:
+        print('|Inserting into "{0}" with columns"{1}"'.format(name, ' '.join(columns)))
+        pprint(data)
     requests.post(influx_host, data=json.dumps([data, ]))
