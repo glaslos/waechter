3dfa94e255cb81ae4991056a4fb35c9c09d021f4
glaslos <lukas.rist@bluecoat.com>
2015-02-24 12:48:49 +0100

    minor formatting fix

--- waechter/utils.py
+++ waechter/utils.py
@@ -11,7 +11,7 @@ def report_data(data_points, name, columns, influx_host='localhost', debug=False
         'points': data_points
     }
     if debug:
-        print('|Inserting into "{0}" with columns"{1}"'.format(name, ' '.join(columns)))
+        print('Inserting into: "{0}" with columns: "{1}"'.format(name, ', '.join(columns)))
         pprint(data)
     if not dry_run:
         requests.post(influx_host, data=json.dumps([data, ]))
