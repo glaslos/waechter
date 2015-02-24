fbd672749a460da3c1f13cef2d08321e641297c7
glaslos <lukas.rist@bluecoat.com>
2015-02-24 09:59:15 +0100

    hello sample

---
+++ samples/hello_job.py
@@ -0,0 +1,25 @@
+import waechter.scheduler
+
+
+class HelloJob1(waechter.scheduler.BaseJob):
+    def __init__(self, interval=None):
+        super(HelloJob1, self).__init__(interval)
+        self.interval = interval if interval else 1
+
+    @classmethod
+    def work(cls):
+        print 'hello work 1'
+
+
+class HelloJob2(waechter.scheduler.BaseJob):
+    def __init__(self, interval=None):
+        super(HelloJob2, self).__init__(interval)
+        self.interval = interval if interval else 2
+
+    @classmethod
+    def work(cls):
+        print 'hello work 2'
+
+
+if __name__ == '__main__':
+    main_worker = waechter.scheduler.JobScheduler().run()
